3eda14f1ec147955d4ef0fd33121a93c94730bef
Alexander D. Cardosi <13227184+adcar@users.noreply.github.com>
2019-10-07 11:04:24 -0400

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,13 +1,11 @@
 // ==UserScript==
 // @name         ServiceNow++
 // @namespace    http://tampermonkey.net/
-// @version      0.3.42
+// @version      0.4.0
 // @description  Adds some extra features to ServiceNow that make my life much easier
 // @author       Alexander Cardosi
 // @match        https://globalfoundries.service-now.com/*
 // ==/UserScript==
-
-// TODO: Make it all valid: https://jsdoc.app/about-getting-started.html
 
 /** Special cases. Will be highlighted in yellow. */
 const specialCases = ["TASK0852576", "TASK0843305", "TASK0848093"];
@@ -43,11 +41,10 @@ function main() {
 }
 
 /**
-* Sets the color of the row based on the 12th child. When the 12th child contains my name "Cardosi, Alexander", the color will be marked green.
-* row the HTML row element
+* Sets the color of the row based on the 12th child. When the 12th child contains my name "Cardosi, Alexander", the color will be marked green. Marks special cases yellow
+* @param {HTMLElement} row - The HTML row element
 */
 function setColor(row) {
-
     const children = Array.from(row.children);
 
     children.forEach((child, index) => {
@@ -76,9 +73,9 @@ function getDescriptions(rows) {
 
 /**
 * Sets each description in the array to their respective columns. The array has to be the same length as the number of rows
-* descriptions array of descriptions (strings)
-* rows HTML element of the rows
-* @throws new error if the length of descriptions does not match that of rows
+* @param {(string|Array)} descriptions - array of descriptions
+* @param {HTMLElement} rows - HTML element of the rows
+* @throws Will through an error if the length of descriptions does not match that of rows
 */
 function setDescriptions(descriptions, rows) {
     if (rows.length !== descriptions.length) throw new Error(`Invalid array length of ${descriptions.length}. Does not match number of rows ${rows.length}.`)
@@ -89,8 +86,8 @@ function setDescriptions(descriptions, rows) {
 
 /**
 * Gets the device name from a description.
-* description string
-* @return line containing device name, or null if not found
+* @param {string} description
+* @return string} line containing device name, or null if not found
 */
 function getDevName(description) {
     return getFromRegex(description, "(Computer Model|Hardware options)(.*)");
@@ -98,8 +95,8 @@ function getDevName(description) {
 
 /**
 * Gets the "requested user" name from a description.
-* description string
-* @return line containing user name, or null if not found
+* @param {string} description
+* @return {string} line containing user name, or null if not found
 */
 function getUserName(description) {
     return getFromRegex(description, "(Open request for this user)(.*)");
@@ -107,9 +104,9 @@ function getUserName(description) {
 
 /**
 * Gets a result from a regex. Returns null if no match found
-* string string that the regex will match on
-* regex pattern that you want to test for
-* @return line containing regex, or null if not found
+* @param {string} string - The string that the regex will perform a match on
+* @param {string} regex - Pattern that you want to test for
+* @return {string} line containing regex, or null if not found
 */
 function getFromRegex(string, regex) {
     const result = string.match(regex);
